0a20c841cb0115baf61fc2b4439dec0feaeb59aa
dpellow <dpellow424@yahoo.ca>
2022-01-24 11:01:44 +0200

    Init

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import itertools
 import numpy as np
 from heapq import merge
 
-import argparse
+import argparse,os
 
 def parse_user_input():
 
@@ -187,8 +187,8 @@ def main(args):
     print("{} negative".format(tot_neg))
 
 
-    not_allowed = set([(m,k) if len(probes_dict[m][k]['negative'][n]) > num_fp for m in probes_dict for k in probes_dict[m] for n in probes_dict[m][k]['negative']])
-    print("{} not allowed:".format(len(not_allowed)))
+    not_allowed = set([(m,k) for m in probes_dict for k in probes_dict[m] for n in probes_dict[m][k]['negative'] if len(probes_dict[m][k]['negative'][n]) > num_fp])
+    print("{} not allowed".format(len(not_allowed)))
 
     # sort the kmers by decreasing number of sequences hit
     kmers_list = [(len(probes_dict[m][k]['positive']),len(probes_dict[m][k]['negative']),m,k) for m in probes_dict for k in probes_dict[m] if (m,k) not in not_allowed]
@@ -200,25 +200,28 @@ def main(args):
 
 
     # TODO: THIS SHOULD BE A FUNCTION
-    covered_targets = set()
+    covered_targets = []
+    covered_targets_set = set()
     ordered_probes =  []
 
     hits_dict = {}
     neg_hits_dict = {}
 
-    for kmer in kmers_list:
+    for i,kmer in enumerate(kmers_list):
         pos_positions = probes_dict[kmer[2]][kmer[3]]['positive']
         neg_positions = probes_dict[kmer[2]][kmer[3]]['negative']
+        # TODO - this should be done better, it really should only be not allowed if the previous
+        # were actually taken.
         for negative in neg_positions:
             if negative not in neg_hits_dict:
                 neg_hits_dict[negative] = neg_positions[negative]
             else:
-                neg_hits_dict[negative] = list(merge(neg_hits_dict[negative],neg_positions[negative]))
+                neg_hits_dict[negative] += neg_positions[negative]
                 if len(neg_hits_dict[negative])>num_fp:
                     # Don't add this probe
-                    not_allowed.add(([kmer[2],kmer[3]))
-                    break
-        if ([kmer[2],kmer[3]) in not allowed: continue # was just added
+                    not_allowed.add((kmer[2],kmer[3]))
+
+        if (kmer[2],kmer[3]) in not_allowed: continue # was just added
 
         # first check if the kmer conflicts with already selected ones
         for target in pos_positions:
@@ -227,51 +230,105 @@ def main(args):
             else:
                 for p in pos_positions[target]:
                     to_insert = bisect.bisect_left(hits_dict[target],p)
-                    if (to_insert>0 and p < hits_dict[target][to_insert-1]+k+MIN_DIST) \
-                        or (to_insert<len(hits_dict[target]) and hits_dict[target][to_insert] < p+k+MIN_DIST):
+                    if (to_insert>0 and p < hits_dict[target][to_insert-1]+plen+MIN_DIST) \
+                        or (to_insert<len(hits_dict[target]) and hits_dict[target][to_insert] < p+plen+MIN_DIST):
                         # skip inserting this one - count how many times it is inserted, and if it's more than one - add it to ordered_probes
-                        not_allowed.add(([kmer[2],kmer[3]))
-                    else:
-                         hits_dict[target].insert(to_insert,p)
-                         if len(hits_dict[target]) > num_probes: covered_targets.add(target)
-                         num_hit+=1
+                        not_allowed.add((kmer[2],kmer[3]))
+                        break
+        if (kmer[2],kmer[3]) in not_allowed: continue # was just added
 
         num_hit = 0
+        for target in pos_positions:
+            if target in covered_targets_set: continue
+            for p in pos_positions[target]:
+                to_insert = bisect.bisect_left(hits_dict[target],p)
+                hits_dict[target] = hits_dict[target][:to_insert]+ [p] +hits_dict[target][to_insert:]
+            if len(hits_dict[target]) > num_probes:
+                covered_targets.append((target,kmer))
+                covered_targets_set.add(target)
+            # TODO: WHERE SHOULD THIS BE ??
+            num_hit=1
 
         if num_hit > 0: ordered_probes.append(kmer)
         if len(covered_targets) == len(targets): break
+
+        if i%10000 == 0:
+            print("Processed {} / {} kmers".format(i,len(kmers_list)))
+            print("{} targets covered out of {}".format(len(covered_targets), len(targets)))
 
     # got here without covering everything - start including false positives
     if len(covered_targets) < len(targets):
         print("{} out of {} targets covered without any FPs".format(len(covered_targets),len(targets)))
         # do the same thing but allow FPs
         kmers_list = [(len(probes_dict[k[2]][k[3]]['positive']),len(probes_dict[k[2]][k[3]]['negative']),k[2],k[3]) for k in not_allowed]
-        kmers_list.sort(key=lambda x: len([t for t in hits_dict if t in probes_dict[x[2]][x[3]]['positive']]) # the number of already selected probes it appears on
-        # print(kmers_list[:25])
+        kmers_list.sort(key=lambda x: \
+                            len([t for t in hits_dict if t in probes_dict[x[2]][x[3]]['positive']])) # the number of already selected probes it appears on
         kmers_list.sort(key=lambda x: x[0]-x[1], reverse=True)
         for kmer in kmers_list:
             pos_positions = probes_dict[kmer[2]][kmer[3]]['positive']
             num_hit = 0
             for target in pos_positions:
-
+                if target in covered_targets_set: continue
                 if target not in hits_dict:
                     hits_dict[target] = pos_positions[target] # this is a sorted list
                 else:
                     for p in pos_positions[target]:
                         to_insert = bisect.bisect_left(hits_dict[target],p)
-                        if (to_insert>0 and p < hits_dict[target][to_insert-1]+k+MIN_DIST) \
-                            or (to_insert<len(hits_dict[target]) and hits_dict[target][to_insert] < p+k+MIN_DIST):
+                        if (to_insert>0 and p < hits_dict[target][to_insert-1]+plen+MIN_DIST) \
+                            or (to_insert<len(hits_dict[target]) and hits_dict[target][to_insert] < p+plen+MIN_DIST):
                             # skip inserting this one - count how many times it is inserted, and if it's more than one - add it to ordered_probes
                             pass
                         else:
-                             hits_dict[target].insert(to_insert,p)
-                             if len(hits_dict[target]) > num_probes: covered_targets.add(target)
-                             num_hit+=1
+                             hits_dict[target] = hits_dict[target][:to_insert]+ [p] +hits_dict[target][to_insert:]
+                if len(hits_dict[target]) > num_probes:
+                    covered_targets.append((target,kmer))
+                num_hit=1
             if num_hit > 0: ordered_probes.append(kmer)
             if len(covered_targets) == len(targets): break
 
 
-    # TODO: DEAL WITH DEGENS
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    with open(os.path.join(outdir,"probes_list.tsv"),'w') as o1, open(os.path.join(outdir,"probes_hits.tsv"),'w') as o2:
+
+        neg_hits_dict = {}
+        ind=0
+
+        o1.write("Probe\tNew positive hits\tNew negative hits\n")
+        o2.write("Probe\tPositive hits\t Negative hits\n")
+        for p in ordered_probes:
+
+            probe_seq = ""
+            j=0
+            for i in range(plen):
+                if i in p[2]:
+                    probe_seq += "N"
+                else:
+                    probe_seq += p[3][j]
+                    j+=1
+
+
+            new_neg_hits = set()
+
+            pos_positions = probes_dict[p[2]][p[3]]['positive']
+            neg_positions = probes_dict[p[2]][p[3]]['negative']
+            for t in neg_positions:
+                if t not in neg_hits_dict:
+                    neg_hits_dict[t] = neg_positions[t]
+                else:
+                    list(merge(neg_hits_dict[t],neg_positions[t]))
+                if len(neg_hits_dict[t]) > num_fp:
+                    new_neg_hits.add(t)
+
+            o1.write(probe_seq+"\t")
+            while ind < len(covered_targets) and covered_targets[ind][1]==p:
+                    o1.write(covered_targets[ind][0]+",")
+                    ind += 1
+            o1.write("\t"+",".join(list(new_neg_hits))+'\n')
+
+            o2.write(probe_seq+"\t")
+            o2.write(", ".join(set([k + ": "+ ",".join([str(pos) for pos in pos_positions[k]]) for k in pos_positions]))+'\t')
+            o2.write(",".join(set([k + ": "+ ",".join([str(pos) for pos in neg_positions[k]]) for k in neg_positions]))+'\n')
 
 
 
